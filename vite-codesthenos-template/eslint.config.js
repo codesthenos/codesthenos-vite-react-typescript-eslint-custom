564ac64e08794ae0bab0b37df4c03b3fe0628a7b
codesthenos <codesthenos@gmail.com>
2024-08-13 14:33:53 +0200

    Incluido en el package.json eslint-plugin-react y modificado el eslint.config.js incluyendo dentro de la propiedad languageOptions la propiedad parserOptions con los paths a los tsconfig.json y el tsconfigRootDir, ademas incluimos la propiedad settings react version, el plugin react previamente importado desde eslint-plugint-react, y en rules incluimos las recommended y las jsx-runtime del plugin de react y al final las normas de formateo que por ahora me gustan, en principio template terminado

--- vite-codesthenos-template/eslint.config.js
+++ vite-codesthenos-template/eslint.config.js
@@ -3,24 +3,60 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  settings: { 
+    react: { 
+      version: '18.3',
+    },
   },
   plugins: {
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
+    react,
   },
   rules: {
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'semi': ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'func-call-spacing': ['error', 'never'],
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'always',
+      'asyncArrow': 'always'
+    }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
+    'object-curly-spacing': ['error', 'always'],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'indent': ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { body: 1, parameters: 1 },
+      FunctionExpression: { body: 1, parameters: 1 },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+    }],
   },
 })
